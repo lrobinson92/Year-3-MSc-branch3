1513b515c9a8b2575b21030c286bbe734a8f6547
Leonie.Robinson <lrobinson92@live.co.uk>
2025-03-16 13:51:07 +0000

    can view files on googledrive

--- frontend/src/reducers/googledrive.js
+++ frontend/src/reducers/googledrive.js
@@ -2,7 +2,9 @@ import {
     GOOGLE_DRIVE_LOGIN_SUCCESS, 
     GOOGLE_DRIVE_LOGIN_FAIL, 
     UPLOAD_DOCUMENT_SUCCESS, 
-    UPLOAD_DOCUMENT_FAIL 
+    UPLOAD_DOCUMENT_FAIL,
+    SET_DOCUMENTS,
+    SET_DRIVE_LOGGED_IN 
 } from '../actions/types';
 
 const initialState = {
@@ -13,8 +15,17 @@ const initialState = {
 
 export default function(state = initialState, action) {
     const { type, payload } = action;
-
     switch (type) {
+        case SET_DOCUMENTS:
+            return {
+                ...state,
+                documents: payload
+            };
+        case SET_DRIVE_LOGGED_IN:
+            return {
+                ...state,
+                driveLoggedIn: payload
+            };
         case GOOGLE_DRIVE_LOGIN_SUCCESS:
             return {
                 ...state,
